c6668aa84d1580e9f65daeea599e6d78fcd4c2f6
dvegap95 <dvegap95@gmail.com>
2022-05-04 18:02:51 -0400

    tab component created

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,17 @@ import './App.css'
 import SelectComponent from './components/SelectComponent'
 import "@fontsource/roboto";
 import { useState } from 'react';
+import TabComponent from './components/TabComponent';
 
 function App() {
   const [selection,setSelection] = useState({id:0});
+  const [tab,setTab] = useState({id:0});
   return (
+    <>
+    <TabComponent value={tab} onChange={setTab}/>
     <SelectComponent value={selection} onChange={setSelection}></SelectComponent>
+    
+    </>
   )
 }
 
--- src/components/TabComponent.tsx
+++ src/components/TabComponent.tsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import React from "react";
+import styles from "./styles/TabComponent.module.css";
 
-export default function TabComponent() {
+export default function TabComponent(props: {
+  value: { id: number };
+  onChange: Function;
+}) {
+  const btns = [{ text: "All" }, { text: "My faves" }];
   return (
-    <div>TabComponent</div>
-  )
+    <div className={styles.tabContainer}>
+      {btns.map((btn, index) => (
+        <div
+          className={styles.tabBtn}
+          onClick={() => {
+            props.onChange({ ...btn, id:index });
+          }}
+          style={
+            props.value.id === index
+              ? {
+                  color: "#1797ff",
+                  borderColor: "#1797ff",
+                }
+              : {}
+          }
+        >
+          <span className={styles.tabBtnText}>{btn.text}</span>
+        </div>
+      ))}
+    </div>
+  );
 }
